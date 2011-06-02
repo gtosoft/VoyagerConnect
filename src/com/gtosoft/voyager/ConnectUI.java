5a9150f6c1670ef31d0563ee937a63b806936178
Brad Hein <gtosoft@gmail.com>
2011-06-01 20:24:47 -0400

    cleanup

--- src/com/gtosoft/voyager/ConnectUI.java
+++ src/com/gtosoft/voyager/ConnectUI.java
@@ -3,22 +3,6 @@ package com.gtosoft.voyager;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * 
- Next steps:
-
-  mkdir VoyagerConnect
-  cd VoyagerConnect
-  git init
-  touch README
-  git add README
-  git commit -m 'first commit'
-  git remote add origin git@github.com:gtosoft/VoyagerConnect.git
-  git push -u origin master
-      
- *
- */
-
 public class ConnectUI extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
